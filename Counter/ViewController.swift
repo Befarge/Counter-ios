1f49e73df734dce7450f6015de94bed7fef7c68c
= <venikov2011@yandex.ru>
2025-02-13 22:46:27 +0300

    full functional

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    private var currentData: String {
+        let formatterDate = DateFormatter()
+        formatterDate.dateFormat = "dd.MM.YYYY HH:mm"
+        return formatterDate.string(from: Date())
+    }
     private var count: Int = 0
     
     override func viewDidLoad() {
@@ -16,10 +22,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func cliickButton() {
+    @IBAction func plusButton() {
+        //check on max value
+        guard count != Int.max else {
+            historyTextView.text += "\n\(currentData) попытка увеличить значение счётчика выше максимума"
+            return
+        }
+        
         count += 1
         counterLabel.text = String(count)
+        historyTextView.text += "\n\(currentData) значение изменено на +1"
+    }
+    
+    @IBAction func minusButton() {
+        //check on min value
+        guard count != 0 else {
+            historyTextView.text += "\n\(currentData) попытка уменьшить значение счётчика ниже 0"
+            return
+        }
+        
+        count -= 1
+        counterLabel.text = String(count)
+        historyTextView.text += "\n\(currentData) значение изменено на -1"
+    }
+    
+    @IBAction func resetButton() {
+        count = 0
+        counterLabel.text = String(count)
+        historyTextView.text += "\n\(currentData) значение сброшено"
     }
     
 }
+
 
