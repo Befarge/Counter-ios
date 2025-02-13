7e392d5230ae1d36e5e3553158dc14aa491fac24
= <venikov2011@yandex.ru>
2025-02-13 18:27:20 +0300

    main functional

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    private var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cliickButton() {
+        count += 1
+        counterLabel.text = String(count)
+    }
+    
 }
 
